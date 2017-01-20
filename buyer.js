1bf8a4191f3be654772c470bd95bb1d554b0e4f0
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-01-20 22:44:56 +0000

    buyer is fully buying from the garage the effects all the cash flow from buyer bankAccount to the Garage financial report and balance

---
+++ buyer.js
@@ -0,0 +1,19 @@
+var Buyer = function(name, bankAccount){
+  this.name = name;
+  this.bankAccount = bankAccount;
+  this.collection = new Array();
+}
+
+Buyer.prototype = {
+  buyCar: function(car){
+    this.collection.push(car);
+    this.bankAccount -= car.price;
+  },
+  // sellCar: function(car){
+  //   soldCarIndex = this.collection.indexOf(car);
+  //   this.collection.splice(soldCarIndex,1);
+  // }
+
+}
+
+module.exports = Buyer;
--- specs/buyer_spec.js
+++ specs/buyer_spec.js
@@ -1,11 +1,18 @@
 var assert = require ("assert");
 var Buyer = require ("../buyer");
 var Car = require ("../car");
+var Garage = require("../garage");
 
 describe("buyer", function(){
   beforeEach(function(){
-    buyer = new Buyer("Winnie");
+    buyer = new Buyer("Winnie", 1000000);
     car1 = new Car ("BMW", "i8", 100000);
+    car2 = new Car("Mclaren", "675LT", 375000);
+    car3 = new Car("Audi", "RS6", 90000);
+    garage = new Garage("Hongs Hot Wheels", "Edinburgh", 100000);
+    garage.addCar(car1);
+    garage.addCar(car2);
+    garage.addCar(car3);
   })
 
   it("buyer should have name", function(){
@@ -13,7 +20,13 @@ describe("buyer", function(){
   })
 
   it("buyer can buy a car", function(){
+    assert.equal(3, garage.inventory.length);
     buyer.buyCar(car1);
+    garage.sellCar(car1);
     assert.equal(1, buyer.collection.length);
+    assert.equal(900000, buyer.bankAccount);
+    assert.equal(2, garage.inventory.length);
+    assert.equal(200000, garage.balance);
+    assert.equal(665000, garage.financialReport());
   })
 });
